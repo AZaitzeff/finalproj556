e360c2c012891738d45a3b70197e5df77021e1c7
Alexander Zaitzeff <azaitzeff@gmail.com>
2018-03-23 15:06:13 -0400

    comments

--- logbarrier.m
+++ logbarrier.m
@@ -1,4 +1,6 @@
 function d=logbarrier(Z,y,d,mu,D,t0)
+%Solves ||y-Zd||_2^2 with ||d_k||_2^2 <=1 where each d_k is dimension D. set mu =100 and t0=100 
+%and if that does not work try mu =10 and t0=10
 eps=.001;
 t=t0;
 MAXITER=1000;
